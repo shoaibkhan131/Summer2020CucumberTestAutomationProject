1c606b29f5bf2c9b8f1b4ffdef969555edf7d3fb
shoaibkhan131 <shoaibkhan131@gmail.com>
2020-11-12 21:00:50 -0500

    updated all codes 11/12/2020

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@smoke",
+        tags = "",
         publish = true
 
 )
